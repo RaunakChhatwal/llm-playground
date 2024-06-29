9fde63b8fece9e2c3f0111131f8ad9f36545e36b
RaunakChhatwal <RaunakChhatwal001@gmail.com>
2024-06-29 13:41:13 -0400

    overhauled history page template

--- ui/src/history.rs
+++ ui/src/history.rs
@@ -1,18 +1,22 @@
 use leptos::*;
-use crate::util::{Menu, Button};
+use crate::util::{Button,ErrorMessage, Menu};
 
 #[component]
 pub fn History(menu: ReadSignal<Menu>, set_menu: WriteSignal<Menu>) -> impl IntoView {
+    let (error, set_error) = create_signal("".to_string());
+
     let to_hide = create_signal(false).0.into();
+
     view! {
-        <div class="relative flex flex-col h-full text-[0.9rem]"
+        <div class="relative flex flex-col items-center mx-auto md:w-[max-content] md:min-w-[60vw]
+                    h-full p-4 md:p-[5vh] overflow-y-hidden text-[0.95em]"
                 style:display=move || (menu.get() != Menu::History).then(|| "None")>
-            <div class="absolute top-4 left-[3vw] flex flex-col items-center w-[94vw]">
-                <Button class="mr-auto" label="Back" to_hide
-                    on_click=Box::new(move || set_menu(Menu::Menu)) />
-                <h1 class="text-[1.25em]">"Conversations"</h1>
-            </div>
-            <div class="grid grid-cols-[50vw] gap-8 justify-center itme-center my-auto">
+            <Button class="mr-auto" label="Back" to_hide
+                on_click=Box::new(move || set_menu(Menu::Menu)) />
+            <h1 class="text-[1.25em]">"History"</h1>
+            <div class="w-full mt-2"><ErrorMessage error /></div>
+            <div class="grid grid-cols-[repeat(2,max-content)] gap-[6.5vh]
+                    items-center my-auto overflow-y-auto">
             </div>
         </div>
     }
